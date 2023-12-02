c82045f1eab5bf25490ec8f1645cfaeb91c1dd40
Daniel Park <parkd7@rpi.edu>
2023-12-01 21:34:44 -0600

    main.cpp Comment

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,13 @@ int main(int argc, char* argv[]) {
     std::cout << "CPU Simulator" << std::endl << std::endl;
 
     if (argc == 1) {
-
+        // Without opening a file.
     } else if (argc == 2) {
-
+        // With opening a file.
     } else {
         std::cout << "Invalid Number of Arguements" << std::endl;
         std::cout << "0 Arguement: Run CPU Simulator without opening a file." << std::endl;
-        std::cout << "1 Arguement: Run CPU simulator with opening a file." << std::endl;
+        std::cout << "1 Arguement: Run CPU simulator with opening a file." << std::endl << std::endl;
 
         return EXIT_FAILURE;
     }
