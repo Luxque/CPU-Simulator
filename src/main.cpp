90b6fddb400fe090d384e71e61acd34cbf153810
Daniel Park <parkd7@rpi.edu>
2023-12-03 17:20:14 -0600

    File I/O

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+#include <fstream>
 
 
 int main(int argc, char* argv[]) {
     std::cout << "CPU Simulator" << std::endl << std::endl;
 
     if (argc == 1) {
-        // Without opening a file.
+        // Simulation w/o file begins here.
     } else if (argc == 2) {
-        // With opening a file.
+        std::ifstream input_file;
+
+        input_file.open(argv[1]);
+        if (!input_file.is_open()) {
+            std::cout << "Failed opening " << argv[1] << "." << std::endl << std::endl;
+            return EXIT_FAILURE;
+        } else {
+            std::cout << "Successfully opened " << argv[1] << "." << std::endl << std::endl;
+        }
+
+        // Simulation w/ file begins here.
+
+        input_file.close();
     } else {
         std::cout << "Invalid Number of Arguements" << std::endl;
         std::cout << "0 Arguement: Run CPU Simulator without opening a file." << std::endl;
